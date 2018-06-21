151e15989427beb9917c35635466da669ab12354
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-06-21 09:33:17 -0700

    update rust mongo test

--- src/hello_mongoc.c
+++ src/hello_mongoc.c
@@ -1,15 +1,18 @@
 //#include <mongoc.h>
 
 extern void random_test();
+extern void init_db();
+
 int
 main (int argc, char *argv[])
 {
-   //mongoc_client_t *client;
+    //mongoc_client_t *client;
 
-   /*
+    /*
     *     * Required to initialize libmongoc's internals
     *         */
-   //mongoc_init ();
+    //mongoc_init ();
+    init_db();
     random_test();
-   return 0;
+    return 0;
 }
