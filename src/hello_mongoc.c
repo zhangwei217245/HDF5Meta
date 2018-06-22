3c29300e7a0c54dbfd0c6071fac83d3815fc4007
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-06-22 09:43:30 -0700

    update mongo test

--- src/hello_mongoc.c
+++ src/hello_mongoc.c
@@ -1,18 +1,39 @@
 //#include <mongoc.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "../lib/hdf5_meta.h"
 
 extern void random_test();
-extern void init_db();
+extern int64_t init_db();
+extern int32_t import_json_to_mongo(const char *json_str);
+
+
+void print_usage() {
+    printf("Usage: ./hdf5_reader /path/to/hdf5/file\n");
+}
 
 int
 main (int argc, char *argv[])
 {
-    //mongoc_client_t *client;
-
-    /*
-    *     * Required to initialize libmongoc's internals
-    *         */
-    //mongoc_init ();
-    init_db();
+    int64_t doc_count = init_db();
+    printf("successfully init db, %d documents in mongodb.\n", doc_count);
+    
     random_test();
+
+    char* filename;
+
+    if (argc != 2)
+        print_usage();
+    else {
+        filename = argv[1];
+        char *json_str = NULL;
+        parse_hdf5_meta_as_json_str(filename, &json_str);
+        printf("%s\n", json_str);
+    }
+
+    int32_t import_rst = import_json_to_mongo(json_str);
+    printf("import_rst = %d\n");
+
     return 0;
 }
