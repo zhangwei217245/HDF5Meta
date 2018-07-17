0cdf84429f53931c5c6c6f348bc15a657f018108
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-07-17 16:23:47 -0700

    test scan hdf5

--- src/test_unindexed_hdf5.c
+++ src/test_unindexed_hdf5.c
@@ -213,7 +213,7 @@ attr_info(hid_t loc_id, const char *name, const H5A_info_t *ainfo, void *opdata)
     //   printf("The value of the attribute with index 2 is:\n");
       j=0;
       for (i=0; i<totsize; i++) {
-        printf ("%s", string_out);
+        printf ("%c", string_out[i]);
         if (j==3) {
           printf(" ");
           j=0;
