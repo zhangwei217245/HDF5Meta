47ffcbae47f4a1499d1bf4cdd882c5ae9a53ba0d
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-09-28 15:49:00 -0500

    try insert many

--- src/HDF5_set_2_mongo.c
+++ src/HDF5_set_2_mongo.c
@@ -61,8 +61,9 @@ int parse_single_file(char *filepath) {
         json_object *sub_group_object = json_object_array_get_idx(root_array, idx);
         json_object_object_add(sub_group_object, "hdf5_filename", 
             json_object_new_string(basename(filepath)));
-        importing_json_doc_to_db(json_object_to_json_string(sub_group_object));
-        
+        char *json_doc = json_object_to_json_string(sub_group_object);
+        importing_json_doc_to_db(json_doc);
+        free(json_doc);
         // json_object *attrs = NULL;
         // json_object_object_get_ex(sub_group_object, "attributes", &attrs);
         // json_object_put(attrs);
@@ -77,7 +78,7 @@ int parse_single_file(char *filepath) {
     printf("[IMPORT_META] Finished in %ld us for %s, with %ld us for parsing and %ld us for inserting.\n",
         one_file_duration, basename(filepath), parse_file_duration, import_one_doc_duration);
     // free(root_array);
-    json_object_put(rootObj);
+    // json_object_put(rootObj);
     // ******** There is another way which is to pass entire JSON object into insert_many function in Rust *****
     // // TODO: Timing for extracting and importing metadata object
     // // TODO: To confirm that you need to uncomment line #32 in hdf52json.c 
