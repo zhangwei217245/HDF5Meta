299ae01d07b96bc6a0465e8223c973e6e84bcdaa
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-09-28 14:57:03 -0500

    try insert many

--- src/HDF5_set_2_mongo.c
+++ src/HDF5_set_2_mongo.c
@@ -61,7 +61,9 @@ int parse_single_file(char *filepath) {
         json_object *sub_group_object = json_object_array_get_idx(root_array, idx);
         json_object_object_add(sub_group_object, "hdf5_filename", 
             json_object_new_string(basename(filepath)));
-        importing_json_doc_to_db(json_object_to_json_string(sub_group_object));
+        char *json_doc = json_object_to_json_string(sub_group_object);
+        importing_json_doc_to_db(json_doc);
+        free(json_doc);
         // json_object *attrs = NULL;
         // json_object_object_get_ex(sub_group_object, "attributes", &attrs);
         // json_object_put(attrs);
