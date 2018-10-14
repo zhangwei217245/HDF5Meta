c1dc52a1e44f014d0ef54bb2a232422b8d890ab8
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-10-14 14:45:25 -0500

    clean useless code

--- lib/fs_ops.h
+++ lib/fs_ops.h
@@ -4,30 +4,6 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
-
-// typedef enum entry_type {
-//     DIR_ENTRY, FILE_ENTRY, UNKNOWN_ENTRY
-// } entry_type_t;
-
-/**
- * When depth = 0, the entry is equal to the given dir specified by given dir path.
- */
-// typedef struct dir_entry {
-//     char *name;
-//     char *canonical_path;
-//     int depth;
-//     entry_type_t dir_type;
-
-//     struct dir_entry *subdir_entries;
-//     struct dir_entry *next;
-//     struct dir_entry *head;
-//     struct dir_entry *tail;
-
-// } dir_entry_t;
-
-// int init_dir_entry(const char *path, dir_entry_t *start_dir);
-// int deinit_dir_entry(dir_entry_t *entry);
-
 typedef enum {
     ASC, DESC
 }sorting_direction_t;
