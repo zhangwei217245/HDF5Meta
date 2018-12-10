fe496883db249d29cb0fdcfa692655f0a8d0fc1f
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-12-09 22:42:53 -0600

    remove comment

--- lib/index/hdf52index.h
+++ lib/index/hdf52index.h
@@ -5,17 +5,6 @@
 #include "../hdf5/new_hdf5_meta.h"
 
 
-/**
- * The content of value tree leaf node should be a hashmap. 
- * The keys of this hashmap are the file paths 
- * The value of each key should be a set of object IDs.
- */
-
 void parse_hdf5_file(char *filepath, index_anchor *idx_anchor);
-
-
-
-
-
 
 #endif /* !MIQS_HDF5_INDEX */
