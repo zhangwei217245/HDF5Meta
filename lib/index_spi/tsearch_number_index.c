e821f6ec8bf4bd2334069cbed68489df52238d3b
Wei Zhang <X-Spirit.zhang@ttu.edu>
2019-03-19 09:16:10 -0400

    update number index

--- lib/index_spi/tsearch_number_index.c
+++ lib/index_spi/tsearch_number_index.c
@@ -20,7 +20,7 @@ int insert_number_to_tsearch_index(void *index_root, void *key, void *data){
     if (index_root == NULL){
         return rst;
     }
-    void *tsearch_rst = tsearch(key, index_root, tsearch_cmp_cb);
+    void *tsearch_rst = tsearch(key, &index_root, tsearch_cmp_cb);
     return tsearch_rst != NULL;
 }
 
@@ -29,7 +29,7 @@ int search_number_from_tsearch_index(void *index_root, void *key, void **out){
     if (index_root == NULL){
         return rst;
     }
-    void *tfind_rst = tfind(key, index_root, tsearch_cmp_cb);
+    void *tfind_rst = tfind(key, &index_root, tsearch_cmp_cb);
     rst = tfind_rst != NULL;
     if (rst) {
         out[0] = tfind_rst;
