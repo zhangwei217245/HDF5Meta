6f2a3a7b91922659585ec658f7f74eca594757ef
Wei Zhang <X-Spirit.zhang@ttu.edu>
2019-03-19 07:43:30 -0400

    update number index

--- lib/index_spi/skiplist_number_index.h
+++ lib/index_spi/skiplist_number_index.h
@@ -2,6 +2,7 @@
 #define MIQS_SKIPLIST_NUMBER_INDEX_H
 
 #include "../libhl/skiplist.h"
+// #include <stdlib.h>
 
 int create_skiplist_index(void **idx_ptr);
 
@@ -9,6 +10,6 @@ int insert_number_to_skiplist(void *index_root, void *key, size_t ksize,void *da
 
 int search_number_from_skiplist(void *index_root, void *key, size_t ksize, void **out);
 
-size_t get_mem_in_skiplist()
+size_t get_mem_in_skiplist();
 
 #endif //end MIQS_SKIPLIST_NUMBER_INDEX_H
