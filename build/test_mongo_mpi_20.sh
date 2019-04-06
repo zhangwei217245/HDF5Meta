9dc6edb2e40ef7e078a0d0df0884c8ecfe944a12
Wei Zhang <X-Spirit.zhang@ttu.edu>
2019-04-05 23:49:26 -0400

    update mpi script

--- build/test_mongo_mpi_20.sh
+++ build/test_mongo_mpi_20.sh
@@ -4,8 +4,8 @@
 #SBATCH -q regular
 #SBATCH -N 20
 #SBATCH --gres=craynetwork:2
-#SBATCH --time-min=02:0:00 
-#SBATCH --time=2:0:00
+#SBATCH --time-min=00:10:00 
+#SBATCH --time=4:0:00
 #SBATCH -L SCRATCH
 #SBATCH -C haswell
 #SBATCH -J INSERT_MONGO_20
@@ -23,7 +23,7 @@ COUNT=10
 ATTRNUM=16
 TASK=0;
 
-PROC_CMD="--cpu_bind=cores -c 64 --mem=40960 --gres=craynetwork:1"
+PROC_CMD="--cpu_bind=cores --ntasks-per-node=1 -c 1 --mem=40960 --gres=craynetwork:1"
 
 PROC=/global/homes/w/wzhang5/software/HDF5Meta/build/bin/hdf5_set_2_mongo
 
