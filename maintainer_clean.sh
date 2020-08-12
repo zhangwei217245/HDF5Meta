9349881499726f47c623d858ac3602bd03836cd8
Wei Zhang <X-Spirit.zhang@ttu.edu>
2020-08-12 10:44:50 -0400

    refactoring structure

--- maintainer_clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-make clean
-make -k maintainer-clean
-rm -rf Makefile.in Makefile build-aux/ autom4te.cache/ aclocal.m4 configure.scan configure config.status config.log m4 "test"
-find ./ -name "Makefile.in" -exec rm -f {} \;
-
-
