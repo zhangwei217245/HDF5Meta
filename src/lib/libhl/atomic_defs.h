c2f4ef7d83c8ab439faf7a4fd52583e768d967f1
Wei Zhang <X-Spirit.zhang@ttu.edu>
2020-08-24 02:29:09 -0400

    update concurrent index code

--- src/lib/libhl/atomic_defs.h
+++ src/lib/libhl/atomic_defs.h
@@ -1,6 +1,8 @@
 #ifndef HL_ATOMIC_DEFS_H
 #define HL_ATOMIC_DEFS_H
 
+
+
 #define ATOMIC_READ(_v) __sync_fetch_and_add(&(_v), 0)
 #define ATOMIC_INCREMENT(_v) (void)__sync_fetch_and_add(&(_v), 1)
 #define ATOMIC_DECREMENT(_v) (void)__sync_fetch_and_sub(&(_v), 1)
@@ -22,6 +24,7 @@
         _o = ATOMIC_READ(_v);\
 }
 
+#undef THREAD_SAFE
 
 #ifdef THREAD_SAFE
 
