83bb2f73789cb60724db00da75ff786d87adafae
Wei Zhang <X-Spirit.zhang@ttu.edu>
2020-08-26 10:08:02 -0400

    update concurrent index code

--- src/lib/index/in_mem_index.c
+++ src/lib/index/in_mem_index.c
@@ -339,7 +339,11 @@ power_search_rst_t *numeric_value_search(char *attr_name, void *value_p, size_t 
 #if MIQS_INDEX_CONCURRENT_LEVEL==1
     pthread_rwlock_rdlock(&(idx_anchor->GLOBAL_INDEX_LOCK[attr_name_hval]));
 #elif MIQS_INDEX_CONCURRENT_LEVEL==2
-    pthread_mutex_lock(&(idx_anchor->GLOBAL_MUTEX_LOCK[attr_name_hval]));
+    do {
+        int ret  = pthread_mutex_trylock(&(idx_anchor->GLOBAL_MUTEX_LOCK[attr_name_hval]));
+        printf("ret: %d\n", ret);
+        nanosleep((const struct timespec[]){{0, 500000000L}}, NULL);
+    } while (ret!=0)
 #endif
     power_search_rst_t *prst =(power_search_rst_t *)calloc(1, sizeof(power_search_rst_t));
     prst->num_files=0;
